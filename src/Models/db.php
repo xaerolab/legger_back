f92cfab5dc4a31dd97890a90d61dd426170f066d
sergio <zelgady@gmail.com>
2022-08-11 09:25:00 -0500

    se construye servicio rest para consulta de registros e insersion de datos mediante slimFramework

---
+++ src/Models/db.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use \PDO;
+
+class DB
+{
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = '';
+    private $dbname = 'landing_page';
+
+    public function connect()
+    {
+        $conn_str = "mysql:host=$this->host;dbname=$this->dbname";
+        $conn = new PDO($conn_str, $this->user, $this->pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $conn;
+    }
+}
